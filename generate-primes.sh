ea21f9d7d4b9f930be73abe1d7e81b6b308ca63a
Yusuf Pisan <pisan@uw.edu>
2021-04-10 16:55:23 -0700

    start

---
+++ generate-primes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# generate-primes.sh
+
+touch prime-numbers/2
+touch prime-numbers/3
+touch prime-numbers/5
+touch prime-numbers/7
+touch prime-numbers/11
+touch prime-numbers/13
+touch prime-numbers/17
+touch prime-numbers/19
+
