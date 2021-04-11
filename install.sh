d5810876b5b555bc9e71282ac83988b779465187
Yusuf Pisan <pisan@uw.edu>
2021-04-10 23:14:29 -0700

    valg

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "# ----------------------------------------------------------"
 echo "# Install script"
 echo "# ----------------------------------------------------------"
 sudo apt update
-sudo apt install -y pandoc
+sudo apt install -y valgrind
 echo "# *** END: Install script"
 echo "# ----------------------------------------------------------"
 echo "# Create the hello.txt file"
